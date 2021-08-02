39e11db1f38dbd2e528affcb0e460cfe0cfdcbbd
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-02 13:59:43 +0200

    Without Modules

--- jour8/jour8.js
+++ jour8/jour8.js
@@ -1,8 +1,7 @@
 // 01 Countries
 
-const prompt = require("prompt");
-prompt.start();
-const request = require("request");
+// const countriesNames = [];
+// const request = require("request");
 
 // request.get("https://restcountries.eu/rest/v1/all", function (err, res, body) {
 //   if (err) console.log(err);
@@ -30,42 +29,15 @@ const request = require("request");
 
 // 03 Pokemon
 
-// const request = require("request");
-// function catchPokemon(num) {
-//   request.get(
-//     `https://pokeapi.co/api/v2/pokemon/${num}`,
-//     function (err, res, body) {
-//       if (err) console.log(err);
-//       const data = JSON.parse(body);
-//       const pokemonName = data.forms[0].name;
-//       console.log(`My pokemon at id:${num}, name is ${pokemonName}`);
-//     }
-//   );
-// }
-// catchPokemon(25);
-
-// Jouer Black Jack
-
-// let min = 16,
-//   max = 21,
-//   bankCards = Math.floor(Math.random() * (max - min + 1) + min),
-//   playerCards = 0;
-// function play() {
-//   prompt.get(["choice"], function (error, result) {
-//     if (result.choice === "draw") {
-//       let min = 1,
-//         max = 10;
-//       playerCards += Math.floor(Math.random() * (max - min + 1) + min);
-//       console.log(`Your card worth ${playerCards} is drawn`);
-//     }
-//   });
-// }
-// play();
-
-function reverseDigits(n) {
-  n = String(n);
-  let array = n.split("");
-  array = array.reverse();
-  return array;
+const request = require("request");
+function catchPokemon(num) {
+  request.get(
+    `https://pokeapi.co/api/v2/berry/${num}/`,
+    function (err, res, body) {
+      if (err) console.log(err);
+      JSON.parse(body);
+      console.log(body[num].endpoint);
+    }
+  );
 }
-console.log(reverseDigits(2345));
+catchPokemon(1);
